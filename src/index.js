2e20f5fcb6249db67f96ac72c735873ed27bb6b7
Geekinfo11 <infogeek476@gmail.com>
2021-01-24 12:16:20 +0100

    add home route /

--- src/index.js
+++ src/index.js
@@ -12,22 +12,16 @@ if(process.argv[2] == 'dev'){// go to package.json for more info
 
 const port = process.env.PORT
 
-// // create a middleware, this middleware runs before we run the route handler, this middleware is just a function.
-// // Without middleware: new request -> run route handler
-// // with middleware: new request -> do something -> run route handler
-// app.use((req, res, next)=>{
-//     res.status(503).send('Site is currently down. check back soon!')
-    
-//     // next()  tells the express server that our function is done, run the route handler now, but if we dont specify next()
-//     // the incoming request from the user will be hanging.
-// })
-
-
 // this will execute the mongoose.js to connect to the database
 require('./db/mongoose');
 
 // this line will convert Json data coming from users to Javascript object
 app.use(express.json());
+
+// setup the home route
+app.get('/', (req, res)=>{
+    res.send('<h1>Welcome to the Home page</h1><p>Please make sure to use an API client to use our app</p>')
+})
 
 // this router has all user routes
 const userRouter = require('./routers/user');
